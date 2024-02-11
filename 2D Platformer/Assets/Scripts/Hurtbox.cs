6d251143b48edad5c69752b203062d209300d961
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2024-02-11 00:39:08 -0500

    Fixed it so enemy hurtboxes can only get attacked by 1 hitbox per frame

--- 2D Platformer/Assets/Scripts/Hurtbox.cs
+++ 2D Platformer/Assets/Scripts/Hurtbox.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class Hurtbox : MonoBehaviour 
 {
     public int health = 100;
+    private bool attacked = false;
+    private int takenDamage = 0;
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Attack"){
+        if (other.tag == "Attack" && attacked == false){
             Debug.Log("I got hit by" + other.name + " for " + other.GetComponent<Hitbox>().damage + " damage.");
-            health -= other.GetComponent<Hitbox>().damage;
-            Debug.Log("I now have " + health + " health.");
+            attacked = true;
+            takenDamage = other.GetComponent<Hitbox>().damage;
         }
+    }
+    private void Update(){
+        if (attacked){
+            LowerHealth();
+        }
+    }
+
+    private void LowerHealth(){
+        health -= takenDamage;
+        takenDamage = 0;
+        attacked = false;
     }
 
 }
