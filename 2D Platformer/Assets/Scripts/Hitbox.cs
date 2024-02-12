6e29e26ebcd5a742d41a4434adc0ec5b6a7676e6
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2024-02-11 23:40:46 -0500

    Removed Unused Code
    Removed unused attack code

--- 2D Platformer/Assets/Scripts/Hitbox.cs
+++ 2D Platformer/Assets/Scripts/Hitbox.cs
@@ -4,22 +4,7 @@ using UnityEngine;
 
 public class Hitbox : MonoBehaviour
 {
-    private IHitboxResponder _responder = null;
-    public int damage = 2;
-/*
-    public LayerMask mask;
-    public Vector2 hitboxSize = Vector2.one;
-    public float radius = 0.5f;
-    public Color inactiveColor;
-    public Color collisionOpenColor;
-    public Color collidingColor;
-    
-    private ColliderState _state;
-
-    public Transform attackPoint;
-    public float attackRange = 0.5f;
-*/
-
+    private int damage;
 
    
 
@@ -29,24 +14,6 @@ public class Hitbox : MonoBehaviour
         
     }
 
-    private void Update(){
-    /*
-        if (_state == ColliderState.Closed){
-            Debug.Log("No colliding state here!");
-            return;}
-        Collider[] colliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale/2, Quaternion.identity, mask);
-
-        for(int i = 0; i < colliders.Length; i++){
-            Debug.Log("We hit something!");
-            Collider aCollider = colliders[i];
-            _responder?.collisionedWith(aCollider);
-        }
-    */
-    }
-
-    public void useResponder(IHitboxResponder responder){
-        _responder = responder;
-    }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "EnemyHurtbox"){
@@ -54,37 +21,13 @@ public class Hitbox : MonoBehaviour
         }
     }
 
-/* Unused functions because I currently can't figure out how to use them
-
-    private void OnDrawGizmos(){
-        Gizmos.color = Color.red;
-        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
-        Gizmos.DrawCube(Vector2.zero, new Vector2(attackRange * 2, attackRange * 2));
+    public void setDamage(int _damage){
+        damage = _damage;
     }
 
-    private void checkGizmoColor(){
-        switch (_state){
-            case ColliderState.Closed:
-                Gizmos.color = inactiveColor;
-                break;
-            case ColliderState.Open:
-                Gizmos.color = collisionOpenColor;
-                break;
-            case ColliderState.Colliding:
-                Gizmos.color = collidingColor;
-                break;
-        }
+    public int getDamage(){
+        return damage;
     }
-
-    public void startCheckingCollision(){
-        _state = ColliderState.Open;
-
-    }
-    public void stopCheckingCollision(){
-        _state = ColliderState.Closed;
-        
-    }
-    */
 
 
     
--- 2D Platformer/Assets/Scripts/PlayerAttack.cs
+++ 2D Platformer/Assets/Scripts/PlayerAttack.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    //[SerializeField] private float attackCooldown;
     private Animator anim;
     private Movement playerMovement;
-    //private float cooldownTimer = Mathf.Infinity;
 
-    public int damage;
-    public Hitbox hitbox;
-    public Transform[] transforms;
-    public float[] attackRange;
-    public LayerMask enemyLayers;
+    public int power = 10;
+    public Hitbox[] hitboxes;
     private void Awake(){
         anim = GetComponent<Animator>();
         playerMovement = GetComponent<Movement>();
+        setPower();
 
+    }
+
+    private void setPower(){
+        foreach(Hitbox hitbox in hitboxes){
+            hitbox.setDamage(power);
+        }
     }
 
     private void Update(){
@@ -25,31 +27,13 @@ public class PlayerAttack : MonoBehaviour
             Attack();
         }
 
-        //cooldownTimer += Time.deltaTime;
     }
 
     private void Attack(){
         anim.SetTrigger("attack");
-        /* Unused code from following a tutorial with a different approach
-        for(int i = 0; i < transforms.Length; i++)
-        {
-            Collider2D[] hitenemies = Physics2D.OverlapCircleAll(transforms[i].position, attackRange[i], enemyLayers);
-            foreach(Collider2D Enemies in hitenemies)
-            {
-                Debug.Log("We hit " + Enemies.name);
-            }
-        }
-        */
-        //hitbox.useResponder(this);
-       // hitbox.startCheckingCollision();
-        //Debug.Log("Collision checking starting!");
     }
 
-
-
-
     void ActivateHitbox(){
-        //print(hitbox1);
         this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
 
     }
@@ -58,20 +42,5 @@ public class PlayerAttack : MonoBehaviour
         this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
 
     }
-    /* More unused code from following a tutorial with a different approach
-        private void OnDrawGizmosSelected(){
-        for(int i = 0; i < transforms.Length; i++)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transforms[i].position, attackRange[i]);
-        }
-    }
-    */
-    /*
-    public void collisionedWith(Collider collider){
-        Hurtbox hurtbox = collider.GetComponent<Hurtbox>();
-        hurtbox?.getHitBy(damage);
-    }
-    */
 
 }
