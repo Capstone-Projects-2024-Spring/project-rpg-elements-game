b92912920b0ef82d5a0ea10bc71887be06e3a997
Brandon <tuk16746@temple.edu>
2024-02-16 16:17:40 -0500

    procedural generation spawning levels to a confined ared
    Point A to B is randomized and map will spawn. Bug where a top closed prefab will spawn under a bottom open prefab

--- 2D Platformer/Assets/Scripts/Level Generation.cs
+++ 2D Platformer/Assets/Scripts/Level Generation.cs
@@ -15,6 +15,13 @@ public class LevelGeneration : MonoBehaviour
     private float timeBetweenRoom;
     public float startTimeBetweenRoom = 0.25f;
 
+    public float minX;
+    public float maxX;
+    public float minY;
+    private bool stopGeneration;
+
+    public LayerMask room;
+
 
     // Start is called before the first frame update
     private void Start()
@@ -28,29 +35,88 @@ public class LevelGeneration : MonoBehaviour
 
     private void Move()
     {
-        if(direction == 1 || direction ==2) { 
-            //This will prompt to move right 
-            Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
-            transform.position = newPos;
+        if(direction == 1 || direction ==2) {
+            //This will prompt to move right
+
+            if (transform.position.x < maxX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+                transform.position = newPos;
+
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                direction = Random.Range(1, 6);
+                if(direction == 3)
+                {
+                    direction = 2;
+                }
+                else if(direction == 4)
+                {
+                    direction = 5;
+                }
+            }
+            else
+            {
+                direction = 5;
+            }
         }
         else if(direction == 3 || direction ==4) {
             //This will prompt to move left
-            Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
-            transform.position = newPos;
+
+            if (transform.position.x > minX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+                transform.position = newPos;
+
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                direction = Random.Range(3, 6);
+
+            }
+            else
+            {
+                direction = 5;
+            }
         }
         else if(direction == 5){
             //This will prompt to move down
-            Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
-            transform.position = newPos;
+
+            if (transform.position.y > minY)
+            {
+
+                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
+                if(roomDetection.GetComponent<RoomType>().type != 1&& roomDetection.GetComponent<RoomType>().type != 3)
+                {
+                    roomDetection.GetComponent<RoomType>().RoomDestruction();
+
+                    int randBottomRoom = Random.Range(1, 4);
+                    if(randBottomRoom == 2)
+                    {
+                        randBottomRoom = 1;
+                    }
+                    Instantiate(rooms[randBottomRoom], transform.position, Quaternion.identity);
+                }
+                Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
+                transform.position = newPos;
+
+                int rand = Random.Range(2, 4);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                direction = Random.Range(1, 6);
+            }
+            else
+            {
+                stopGeneration = true;
+            }
         }
 
-        Instantiate(rooms[0], transform.position, Quaternion.identity);
-        direction = Random.Range(1, 6);
     }
     // Update is called once per frame
     void Update()
     {
-        if(timeBetweenRoom <= 0){
+        if(timeBetweenRoom <= 0 && stopGeneration == false){
             Move();
             timeBetweenRoom = startTimeBetweenRoom;
         }
