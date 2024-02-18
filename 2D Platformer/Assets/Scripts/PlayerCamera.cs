07f621b09f12c80030c2a102e513dfa72490b19b
aTran47 <tuk82621@temple.edu>
2024-02-18 12:14:36 -0500

    Changed camera to utilizing lerping to smooth transition between bounds
    Also, the camera seems a bit jittery and I'm not 100% sure why that's the case.

--- 2D Platformer/Assets/Scripts/PlayerCamera.cs
+++ 2D Platformer/Assets/Scripts/PlayerCamera.cs
@@ -6,31 +6,21 @@ using Cinemachine;
 public class PlayerCamera : MonoBehaviour
 {
     private CinemachineVirtualCamera camObj;
+    public float smoothing = 5f;
     CinemachineFramingTransposer comp;
     // Start is called before the first frame update
 
     private void Awake()
     {
         camObj = GetComponent<CinemachineVirtualCamera>();
-    }
-    void Start()
-    {
         comp = camObj.GetComponentInChildren<CinemachineFramingTransposer>();
-        
-
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        if(horizontalInput > 0.01f){
-            comp.m_TrackedObjectOffset.x = 3f;
-        }else if(horizontalInput < -0.01f){
-            comp.m_TrackedObjectOffset.x = -3f;
-        }else{
-            comp.m_TrackedObjectOffset.x = 0f;
-        }
+        float targetXOffset = Mathf.Lerp(comp.m_TrackedObjectOffset.x, horizontalInput * 3f, Time.deltaTime * smoothing);
+        comp.m_TrackedObjectOffset.x = targetXOffset;
     }
 }
