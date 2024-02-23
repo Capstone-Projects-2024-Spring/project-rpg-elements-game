b2c27834ad93702cc1281c8ede99b1c78d9c8224
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2024-02-22 23:21:30 -0500

    Improved Readability
    Added comments explaining the code for each attack script, the hitbox script, and the hurtbox script.

--- 2D Platformer/Assets/Scripts/Attacking/ChargeableDashAttack.cs
+++ 2D Platformer/Assets/Scripts/Attacking/ChargeableDashAttack.cs
@@ -2,42 +2,71 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+    For chargeable attacks that send the player forwards
+    How long they charge the move is how long they hold the button for.
+    How long they charge affects the following:
+    - How far do they go?
+    - How fast are they?
+    - How much damage do they do?
+    - How much knockback do they do?
+    - How long are they charging for?
+*/
 public class ChargeableDashAttack : PlayerAttack
 {
+    // Keeps track of how long the move is charged for.
     private float chargeTimer = 0;
+    /*
+        Determines how long (in seconds) the player has to charge for until they reach the next level
+        of charge.
+        Mid level is the 2nd level of charge.
+        Max level is the 3rd and highest level of charge.
+        If neither of these levels are meet, the attack's default values are used instead.
+    */
     [SerializeField] private float midLevelThreshold = 1.0f;
     [SerializeField] private float maxLevelThreshold = 2.0f;
+    /*
+        Multipliers for when the player reaches certain levels of charge.
+        They amplify the folllowing: Damage, knockback, velocity, acceleration, dashTimer
+        Of course, max level charge should be more intense than mid level.
+    */
     [SerializeField] private float midLevelMultiplier = 1.2f;
 
     [SerializeField] private float maxLevelMultiplier = 1.35f;
-
+    //Decide the acceleration and speed of the player while they're dashing.
     [SerializeField] private float dashAcceleration = 2.0f;
 
     [SerializeField] private float maxVelocity = 10.0f;
-
+    //Decides how long the player is dashing for.
     [SerializeField] private float dashTimer = 0.5f;
+    /*
+        The minimum amount of time (in seconds) the player is required to be charging for.
+        Even if the player releases the attack before this point, they are required to wait
+        this long until the charge is released.
+    */
     [SerializeField] private float startingLag = 0.33f;
 
-
+// "store" variables used for resetting the attack to its default settings.
     private float storeTimer;
     private int storePower;
     private float[] storeKnockback = new float[2];
 
+    private float storeMaxVelocity;
+
+    private float storeAcceleration;
+//booleans used by this script and the animator to determine which phase of the attack the player's in. 
     private bool dashed = false;
 
     private bool stopped = false;
 
     private bool dashStartup = false;
-
+//Multiplied with the other properties to amplify the attack.
     private float multiplier;
 
-    private float storeMaxVelocity;
 
-    private float storeAcceleration;
 
     private SpriteRenderer sprite;
-
+//Sets all of the "store" variables. Oh, and the sprite too.
     protected override void Awake()
     {
         base.Awake();
@@ -51,7 +80,7 @@ public class ChargeableDashAttack : PlayerAttack
         sprite = GetComponent<SpriteRenderer>();
 
     }
-
+//Overrides the parent function so that it detects if the button is being held down, not just pushed.
     protected override bool checkForInput(){
         
         if (!playerMovement.canAttack()){
@@ -74,6 +103,14 @@ public class ChargeableDashAttack : PlayerAttack
         //Debug.Log(hitboxes[0].getDamage());
         
         base.Update();
+
+    //Increments the charge timer based on how long the button is being held.
+        if(Input.GetKey(triggerKey)){
+            chargeTimer += Time.deltaTime;
+
+        }
+
+    //Sets the multiplier and sprite color based on how long the attack is charging for.
             if(chargeTimer < midLevelThreshold){
                 multiplier = 1.0f;
             } 
@@ -88,32 +125,36 @@ public class ChargeableDashAttack : PlayerAttack
                 sprite.color = new Color(1f,0.4196079f, 0.8923197f,1f);
             }
 
-
-        if(Input.GetKey(triggerKey)){
-            chargeTimer += Time.deltaTime;
-
-        }
+    //If the button is released, amplify everything that needs to be amplified.
         if(Input.GetKeyUp(triggerKey)){
             setupDash();
             //Debug.Log(power);
         }
         
-
+    /*
+    The following should ONLY run if the player is currently in the dashing phase.
+    */
         if(!dashed){
             return;
         }
-
+    //Restore default color to the sprite
         sprite.color = new Color(1,1f,1f,1f);
-    
+    /*
+    If the player released the attack before they reached the attack's starting lag, they're forced to
+    charge until they do so. Prevents them from instantaneously dashing.
+    */
         if(chargeTimer < startingLag){
             dashStartup = true;
             chargeTimer += Time.deltaTime;
             return;
         }
+    /*
+        Moves the player forwards and decreases the "dash timer"
+    */
         dashStartup = false;
         dashTimer -= Time.deltaTime;
         dash();
-    
+    //Once the dash timer hits 0, the player stops dashing and everything is reset.
         if(dashTimer <= 0){
             stopped = true;
             dashed = false;
@@ -129,7 +170,7 @@ public class ChargeableDashAttack : PlayerAttack
             //Debug.Log(body.velocity[0]);
 
         }
-
+    //Used by the animator to convey which phase of the attack the player's in.
         anim.SetBool("dashed", dashed);
         anim.SetBool("stopped", stopped);
         anim.SetBool("dashStartup", dashStartup);
@@ -140,7 +181,7 @@ public class ChargeableDashAttack : PlayerAttack
 
         
     }
-
+//Overrides the parent function by also setting acceleration based on which way the player's facing.
     protected override void setKnockbackDirection(){
         if(playerMovement.getDirection() == Direction.left){
             knockback[0] = xKnockbackValue * -1;
@@ -151,7 +192,11 @@ public class ChargeableDashAttack : PlayerAttack
             dashAcceleration = Math.Abs(dashAcceleration);
         }
     }
-
+/*
+    Used by the Update function to multiply everything that needs to be multiplied.
+    Booleans are also changed so Update knows which phase of the attack the player's in.
+    The hitboxes are also set again with these new, amplified values.
+*/
     private void setupDash(){
             power = (int) (power * multiplier);
             knockback[0] *= multiplier;
@@ -163,20 +208,16 @@ public class ChargeableDashAttack : PlayerAttack
             stopped = false;
             dashed = true;
             setHitboxes();
-            Debug.Log(power + " " + hitboxes[0].getDamage());
+            //Debug.Log(power + " " + hitboxes[0].getDamage());
     }
 
-    private void detect_stopping(){
-        if(success){
-            stopped = false;
-        }
-        if(Math.Abs(body.velocity[0]) <= 0.3){
-            stopped = true;
-        }else{
-            stopped = false;
-        }
-    }
-
+/*
+    Used by update to move the player forwards.
+    Unlike with rising attacks, this is called every frame to ensure that there's acceleration to the attack
+    And to ensure that the player will reach their max speed regardless of friction
+    Oh yeah, and the player can't move faster than whatever max velocity is set as (which also depends on
+    how long they've charged the attack for) 
+*/
     private void dash(){
         dashed = true;
         if(Math.Abs(body.velocity[0]) >= maxVelocity){
@@ -185,7 +226,10 @@ public class ChargeableDashAttack : PlayerAttack
         body.AddForce(new Vector2(dashAcceleration, 0), ForceMode2D.Impulse);
         //body.velocity = new Vector2(dashAcceleration, body.velocity[1]);
     }
-
+/*
+    USED BY THE ANIMATOR ONLY
+    Turns off hitboxes and resets the dash boolean
+*/
     private void DeactivateDashing(){
         base.DeactivateHitbox();
         dashed = false;
--- 2D Platformer/Assets/Scripts/Attacking/PlayerAttack.cs
+++ 2D Platformer/Assets/Scripts/Attacking/PlayerAttack.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Unity.Properties;
 
+//The basic attack script that all attacks will inherit from. Contains logic that all attacks should follow.
 public abstract class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private PlayerStats statSheet;
@@ -10,38 +12,62 @@ public abstract class PlayerAttack : MonoBehaviour
     protected Animator anim;
     protected Movement playerMovement;
 
-    [SerializeField] protected int power;
+/* 
+Base damage of the move. Later, attacks will also account for the player's strength stat when
+calculating damage
+*/
+    [SerializeField] protected int power; 
 
+//Base knockback of the move. Unlike with power, this should NOT change based on the player's strength stat
+//First item is knockback in the x direction. The second one is knockback in the y direction.
     [SerializeField] protected float[] knockback = {4, 3};
-
+/* 
+The key that's pressed to trigger the attack.
+Later, when the attack menu is being created, these can be changed in-game based on which attacks the
+player is equipping. For now, before that gets implemented, every attack will have a unique trigger key
+for testing purposes
+ */
     [SerializeField] protected KeyCode triggerKey;
-
+//Knockback in the x direction as a variable. Used to account for player direction when dealing knockback
     protected float xKnockbackValue;
+//Array of which hitboxes will be used by the attack.
     [SerializeField] protected Hitbox[] hitboxes;
-
+//For development purposes. In the final game, all hitboxes will be invisible
     [SerializeField] protected bool visible_hitboxes = true;
-
-    public String attackName = "strike";
-
+    [SerializeField] public String attackName = "strike";
+/*
+For the animator to know which animation to trigger. 
+It's a separate variable from attackName since I'll probably have to capialize the attack names later, but
+not the animation triggers
+ */
     [SerializeField] protected string animationTrigger = "attack";
 
-
+//How many times the attack is used. Used when creating the "Attack ID"
     private int uses = 0;
-
+//The time (in seconds) a player and enemy is frozen for when an attack connects
     private double hitlag;
+//Whether or not the attack hits.
     protected bool success = false;
+/*
+Used to signify that if a specific attack is being used or not. Important for making sure two attacks
+don't trigger at the same time. Shouldn't really be messed with. */
     protected bool active;
 
     private float hitlagTimer = 0;
-
+/*
+Used for getting the ID of who the attack landed on. Important for making sure the attack hits an enemy only
+once when it's active and not on every frame
+*/
     private String receiverID = "";
 
     protected Rigidbody2D body;
-
+/*Used when the player freezes in hitlag to make sure any momentum 
+they had beforehand is preserved for when they unfreeze
+*/
     private Vector2 storedVelocity = new Vector2();
-
+// A boolean to be used by other attacks that would want to limit whether or not an attack could be used
     protected bool other_constraints = true;
-
+//Runs once when the script is started.
     protected virtual void Awake(){
         anim = GetComponent<Animator>();
         playerMovement = GetComponent<Movement>();
@@ -49,7 +75,9 @@ public abstract class PlayerAttack : MonoBehaviour
         xKnockbackValue = Math.Abs(knockback[0]);
         hitlag = setHitlag(knockback[0], knockback[1]);
     }
-
+/*
+    Sets up the following for each hitbox in the attack: Damage, Knockback, Hitlag, Visibility
+*/
     protected void setHitboxes(){
         foreach(Hitbox hitbox in hitboxes){
             hitbox.setDamage(power);
@@ -63,7 +91,13 @@ public abstract class PlayerAttack : MonoBehaviour
             }
         }
     }
-
+/*
+Used in the update function to decide if the player can attack. 
+An attack can only be triggered if the following are true:
+    - Did the player hit the attack button?
+    - Is the player not in an attacking state already?
+    - Any other constraints that should prevent the player from attacking?
+*/
     protected virtual bool checkForInput(){
         if(!Input.GetKeyDown(triggerKey)){
             return false;
@@ -79,6 +113,14 @@ public abstract class PlayerAttack : MonoBehaviour
     }
 
     protected virtual void Update(){
+    /*
+        When an attack is triggered, the following are true:
+         - The attack is now "active". Only one attack can be active at a time
+         - The player is now in an "attacking" state. So they shouldn't be able to run or jump.
+         - The hitboxes in the attack are set
+         - The "attack ID" is set (more information in setAttackName())
+         - The animation for the attack triggers
+    */
         if(checkForInput()){
             playerMovement.setAttackStateTrue();
             active = true;
@@ -86,20 +128,32 @@ public abstract class PlayerAttack : MonoBehaviour
             setAttackName();
             Attack();
         }
-
+    //Reverses knockback if the player is facing left.
         setKnockbackDirection();
+    /*
+        The following code should NOT run UNLESS the attack is active.
+        Consequently, only one attack should be able to access this code at a time.
+    */
         if(!active){
             //Debug.Log(attackName + "is not active");
             return;
         }
-        //Debug.Log(attackName + " is active!");
+        Debug.Log(attackName + " is active!");
+
+    
+    //If the attack manages to hit something, the player should freeze for a short amount of time
+    
         checkSuccess();
         if(success){
             enterHitlag();
             //Debug.Log("This should only print once");
             success = false;
         }
-    
+    /*
+    If the player is frozen after hitting something, a timer starts.
+    Animations are frozen until the timer is greater than how much "hitlag" the attack has.
+    Once the timer reaches that point, the player unfreezes.
+    */
         if(anim.enabled == false){
             hitlagTimer += Time.deltaTime;
             //Debug.Log(hitlagTimer);
@@ -109,7 +163,10 @@ public abstract class PlayerAttack : MonoBehaviour
         }
 
     }
-
+/*
+Sets success to true if the attack has landed.
+Only hits an enemy once until the attack ends.
+*/
     protected void checkSuccess(){
         //Debug.Log(attackName);
         foreach(Hitbox hitbox in hitboxes){
@@ -121,7 +178,12 @@ public abstract class PlayerAttack : MonoBehaviour
         }
 
     }
-
+/*
+    Gives each attack an "ID" based on the attack name and how many times it's been used.
+    Used to ensure that an enemy doesn't get hit by an attack each frame.
+    Once multiplayer is integrated, each player should also have a unique ID, and that ID should
+    be a part of the attack ID as well so that 2 players can never have the same attack ID.
+*/
     protected void setAttackName(){
         uses += 1;
         String attackUses = uses.ToString();
@@ -131,14 +193,20 @@ public abstract class PlayerAttack : MonoBehaviour
             //Debug.Log(hitbox.getAttackID());
         }
     }
-
+/*
+    Calculates hitlag. "Hitlag" is how much time (in seconds) the player and enemy are frozen for 
+    if the attack lands.
+*/
     protected double setHitlag(float xknockback,float yknockback){
         double calculated_hitlag = (Math.Sqrt(Math.Pow(Math.Abs(xknockback), 2) + Math.Pow(Math.Abs(yknockback), 2)) * 0.65 + 6)/60;
         if(calculated_hitlag > 0.5){calculated_hitlag = 0.5;}
         return calculated_hitlag;
 
     }
-
+/*
+    Called by the Update function to freeze the player in place when an attack lands.
+    Any momentum the player had beforehand is stored until they unfreeze.
+*/
     protected void enterHitlag(){
         hitlagTimer = 0;
         anim.enabled = false;
@@ -147,7 +215,9 @@ public abstract class PlayerAttack : MonoBehaviour
         //Debug.Log("Before " + attackName + ": " + storedVelocity);
         
     }
-
+/*
+    Used by the Update function to unfreeze a player once enough time has passed after an attack has landed.
+*/
     protected void exitHitlag(){
         anim.enabled = true;
         body.constraints = RigidbodyConstraints2D.FreezeRotation;
@@ -155,7 +225,10 @@ public abstract class PlayerAttack : MonoBehaviour
         //Debug.Log("After " + attackName + ": " + body.velocity);
         //Debug.Log("No longer in hitlag");
     }
-
+/*
+    Inverts knockback in the x direction if the player is facing left.
+    Ensures that attack knockback behaves the same regardless of if the player is facing left or right.
+*/
     protected virtual void setKnockbackDirection(){
         if(playerMovement.getDirection() == Direction.left){
             knockback[0] = xKnockbackValue * -1;
@@ -164,12 +237,19 @@ public abstract class PlayerAttack : MonoBehaviour
             knockback[0] = xKnockbackValue;
         }
     }
-
+/*
+    Triggers the animation for the attack. In hindsight, this could either be renamed, have more stuff moved
+    into it, or just have this one line of code moved to the Update function and then delete it.
+    But for now it stays.
+*/
     protected void Attack(){
         //Debug.Log("This should only print once");
         anim.SetTrigger(animationTrigger);
     }
-
+/*
+    USED BY THE ANIMATOR ONLY
+    Turns on all hitboxes used by whichever attack is active.
+*/
     protected virtual void ActivateHitbox(){
         if(!active){
             return;
@@ -179,10 +259,13 @@ public abstract class PlayerAttack : MonoBehaviour
         }
 
     }
-
-
-
-
+/*
+    USED BY THE ANIMATOR ONLY
+    Turns OFF all hitboxes and sets "active" to false.
+    Note that this DOESN'T turn off attacking state. That's a separate function used in the movement script.
+    If an attack is NOT active, but the player is STILL in an attacking state, then the player is in
+    the COOLDOWN portion of the attack.
+*/
     protected void DeactivateHitbox(){
         foreach(Hitbox hitbox in hitboxes){
             hitbox.setSuccess(false);
