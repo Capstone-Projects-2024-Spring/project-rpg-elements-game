d1a84b76adb455d74fdb6262e4388712b00fcf5a
aTran47 <tuk82621@temple.edu>
2024-03-02 18:41:50 -0500

    It.... almost works?
    I think the vector created from the wall slide is intefereing with the jump vector

--- 2D Platformer/Assets/Scripts/Movement.cs
+++ 2D Platformer/Assets/Scripts/Movement.cs
@@ -17,6 +17,14 @@ public class Movement : MonoBehaviour
     private Direction facing = Direction.right;
     [SerializeField] private float friction = 1;
 
+    private bool isWallSliding;
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 3.5f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+
     private void Start(){
         //Grabs references for Rigidbody, Box Collider, and Animator
         body = GetComponent<Rigidbody2D>();
@@ -42,19 +50,11 @@ public class Movement : MonoBehaviour
             return;
         }
         //Makes the player move left/right
-
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * statSheet.Speed.Value, body.velocity.y);
 
     
 
-        //Flips sprite when turning left/right
-        if(horizontalInput > 0.01f){
-            transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
-            facing = Direction.right;
-        }else if(horizontalInput < -0.01f){
-            transform.localScale = new Vector3(-0.25f, 0.25f, 0.25f);
-            facing = Direction.left;
-        }
+
         
         //Makes the player jump when space is pressed
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded()){
@@ -64,18 +64,25 @@ public class Movement : MonoBehaviour
         if(onWall() && body.velocity.x != 0){
             boxCollider.sharedMaterial.friction = 0;
             body.velocity = new Vector2(0, -1 * Math.Abs(wall_sliding_speed));
+            isWallSliding = true;
         }
         else{
             boxCollider.sharedMaterial.friction = friction;
+            isWallSliding = false;
         }
-        //Debug.Log(boxCollider.sharedMaterial.friction);
+        WallJump();
 
-
-
-
-    
-
- 
+        //Flips sprite when turning left/right
+        //Is supposed to not turn the player if in the middle of a wall jump
+        if(!isWallJumping){
+            if(horizontalInput > 0.01f){
+                transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+                facing = Direction.right;
+            }else if(horizontalInput < -0.01f){
+                transform.localScale = new Vector3(-0.25f, 0.25f, 0.25f);
+                facing = Direction.left;
+            }
+        }
     }
 
     private void Jump(){
@@ -118,8 +125,46 @@ public class Movement : MonoBehaviour
         boxCollider.sharedMaterial.friction = _friction;
     }
     
+    private void WallJump()
+    {
 
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            //Sets direction opposite of direction of wall
+            wallJumpingDirection = -transform.localScale.x;
+            //Gives time for player to turn away then jump
+            wallJumpingCounter = wallJumpingTime;
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
 
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            Debug.Log("Wall Jumped!");
+            isWallJumping = true;
+            body.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDirection)
+            {
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+
+        
+    }
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
 
 
 }
