2191c6208b1548f2e422724c76a01ca3332970fe
tun50126 <tun50126@temple.edu>
2024-03-20 22:47:16 -0400

    Added UI for Forgot Password and Profile Pages
    Added two new panels to AccountMenu, Forgot Password page and Profile page. Gave them UI but features are not yet functional, only buttons to change pages are.

--- 2D Platformer/Assets/Scripts/FirebaseController.cs
+++ 2D Platformer/Assets/Scripts/FirebaseController.cs
@@ -6,29 +6,82 @@ using UnityEngine.SceneManagement;
 
 public class FirebaseController : MonoBehaviour
 {
-    public GameObject loginPanel,signupPanel;
+    public GameObject loginPanel, signupPanel, profilePanel, forgotPassPanel;
 
-    //public InputField loginEmail, loginPassword, signupEmail, signupPassword, signupConfirm;
+    public InputField loginEmail, loginPassword, signupEmail, signupPassword, signupConfirm, signupIGN;
 
-    public void OpenLoginPanel() {
+    public void OpenLoginPanel()
+    {
 
         loginPanel.SetActive(true);
         signupPanel.SetActive(false);
+        profilePanel.SetActive(false);
+        forgotPassPanel.SetActive(false);
     }
 
-    public void OpenSignupPanel() {
+    public void OpenSignupPanel()
+    {
 
         loginPanel.SetActive(false);
         signupPanel.SetActive(true);
+        profilePanel.SetActive(false);
+        forgotPassPanel.SetActive(true);
     }
 
-    public void LastScene() 
+    public void OpenProfilePanel()
+    {
+
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(true);
+        forgotPassPanel.SetActive(true);
+    }
+
+    public void OpenForgotPassPanel()
+    {
+
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(false);
+        forgotPassPanel.SetActive(true);
+    }
+
+    public void LastScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
-    public void NextScene() 
+    public void NextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void Login()
+    {
+        if (string.IsNullOrEmpty(loginEmail.text) && string.IsNullOrEmpty(loginPassword.text))
+        {
+            return;
+        }
+
+        // login code here
+    }
+
+    public void Signup()
+    {
+        if (string.IsNullOrEmpty(signupEmail.text) && string.IsNullOrEmpty(signupPassword.text) && string.IsNullOrEmpty(signupConfirm.text) && string.IsNullOrEmpty(signupIGN.text))
+        {
+            return;
+        }
+
+        // signup code here
+
+    }
+
+    // public void forgotPassword()
+    // {
+    //     if (string.IsNullOrEmpty(forgetPassEmail.text))
+    //     {
+    //         return;
+    //     }
+    // }
 }
