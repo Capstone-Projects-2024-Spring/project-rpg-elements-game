fef2dadd6c436eb310cf8131b16d988f2f82fa37
amcginn92 <tug74324@temple.edu>
2024-03-23 15:20:54 -0400

    Creating MenuManager system
    Created a system to manage the menus to ensure only one is open at a time, and to facilitate selection of menu on navigation.

---
+++ 2D Platformer/Assets/Scripts/Menu/Launcher.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Connecting to Master");
+        //PhotonNetwork.ConnectUsingSettings();
+    }
+}
