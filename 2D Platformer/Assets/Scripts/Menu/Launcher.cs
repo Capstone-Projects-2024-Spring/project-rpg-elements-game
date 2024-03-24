9da8f7b842ca2a1259d00cfee3c8d459ca07ae14
amcginn92 <tug74324@temple.edu>
2024-03-24 10:48:11 -0400

    Added logic for creating/Joining a room
    Menu now will go to a lobby creation menu from play game button. From there you can create a name for your room and create it.

--- 2D Platformer/Assets/Scripts/Menu/Launcher.cs
+++ 2D Platformer/Assets/Scripts/Menu/Launcher.cs
@@ -1,9 +1,14 @@
+using Codice.Client.BaseCommands;
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
 
 //MBPC: Class provides .photon view and callbacks/events that pun can call
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    [SerializeField] InputField roomNameInputField;
+    [SerializeField] Text roomNameText;
+    [SerializeField] Text errorText;
     private void Start()
     {
         Debug.Log("Connecting to Master");
@@ -21,5 +26,43 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         MenuManager.instance.OpenMenu("MainMenu");
         Debug.Log("Joined Lobby");
+    }
+    
+    public void CreateRoom()
+    {
+        //if the roomname is null we return
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+
+        //otherwise we create the room
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        //show load menu
+        MenuManager.instance.OpenMenu("LoadingMenu");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //open RoomMenu menu
+        MenuManager.instance.OpenMenu("RoomMenu");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string errorMessage)
+    {
+        errorText.text = "Room Generation Unsuccessful" + errorMessage;
+        MenuManager.instance.OpenMenu("ErrorMenu");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.instance.OpenMenu("LoadingMenu");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.instance.OpenMenu("MainMenu");
     }
 }
