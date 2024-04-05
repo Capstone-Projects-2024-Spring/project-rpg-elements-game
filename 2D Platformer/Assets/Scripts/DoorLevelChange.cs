fc51ddf9383c40f87c08254acb701f36e1cae381
Brandon <tuk16746@temple.edu>
2024-04-05 13:02:36 -0400

    Changing Door Logic

--- 2D Platformer/Assets/Scripts/DoorLevelChange.cs
+++ 2D Platformer/Assets/Scripts/DoorLevelChange.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Mirror;
+using System;
 
-public class Door : MonoBehaviour
+public class Door : NetworkBehaviour
 {
+    public Vector2 bossPosition;
+
+    public GameObject player;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            print("Switching Scene");
+            print("Switching Locations");
 
-            SceneManager.LoadScene("Boss");
+            movePlayer(bossPosition);
         }
     }
-    
+
+    [ClientRpc]
+    private void movePlayer(Vector2 bossPosition)
+    {
+        player.transform.position = bossPosition;
+    }
 }
