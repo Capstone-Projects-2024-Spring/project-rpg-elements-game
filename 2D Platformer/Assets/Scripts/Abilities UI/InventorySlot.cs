9160a8131fd383dfd766addaccbe0da65a928090
Brian <tun40876@temple.edu>
2024-04-07 18:21:36 -0400

    Click and drag UI with images

---
+++ 2D Platformer/Assets/Scripts/Abilities UI/InventorySlot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject dropped = eventData.pointerDrag;
+        DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+        draggableItem.parentAfterDrag = transform;
+    }
+}
