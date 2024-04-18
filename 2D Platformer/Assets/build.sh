632e8f616f4af8ef7ab064c75d5fc8ee6982412d
amcginn92 <tug74324@temple.edu>
2024-04-18 10:42:00 -0400

    Added server build script
    Created a script to quickly build linux server. Was taking a bit too long to switch between platforms etc so this should speed things up

---
+++ 2D Platformer/Assets/build.sh
@@ -0,0 +1,3 @@
+echo "Starting build..."
+"C:\Program Files\2022.3.17f1\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\amcgi\Documents\GitHub\Wildlife\project-rpg-elements-game" -executeMethod BuildScript.BuildAll
+echo "Finished build!"
