14dce4c949da5cd44c3667c10c1fda372c56e29a
aTran47 <tuk82621@temple.edu>
2024-04-27 13:56:01 -0400

    fix flipping and sorta fixed position

--- 2D Platformer/Assets/nametag.cs
+++ 2D Platformer/Assets/nametag.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class NameTagController : MonoBehaviour
 {
     public Transform playerTransform; // Reference to the player's transform
+    public Transform thisTransform;
     public Vector3 offset = new Vector3(0f, 2f, 0f); // Offset to position the name tag above the player
 
     void Update()
@@ -12,6 +13,7 @@ public class NameTagController : MonoBehaviour
         {
             // Update the position of the name tag relative to the player's position
             transform.position = playerTransform.position + offset;
+            thisTransform.localScale = new Vector3(playerTransform.localScale.x, thisTransform.localScale.y, thisTransform.localScale.z);
         }
     }
 }
