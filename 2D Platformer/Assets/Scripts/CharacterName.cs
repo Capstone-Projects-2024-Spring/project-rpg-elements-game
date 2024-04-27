1b13208fc791d28cf1e62c650f4559249f4f2496
Brandon Cuevas <tuk16746@temple.edu>
2024-04-27 12:32:21 -0400

    Added needed scripts

---
+++ 2D Platformer/Assets/Scripts/CharacterName.cs
@@ -0,0 +1,17 @@
+using Mirror;
+using TMPro;
+using UnityEngine;
+
+public class CharacterName : NetworkBehaviour
+{
+    private string playerName = "";
+
+    [SerializeField] private TMP_Text playerNameText = default;
+
+
+    public void SetPlayerName(string newName)
+    {
+        playerName = newName;
+        playerNameText.text = playerName;
+    }
+}
