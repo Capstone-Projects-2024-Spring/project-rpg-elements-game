15a1a7c110e10272c8f4638f1d3d5ba400878524
Brandon Cuevas <tuk16746@temple.edu>
2024-04-27 12:45:23 -0400

    Characters names arent overlapped

--- 2D Platformer/Assets/Scripts/CharacterName.cs
+++ 2D Platformer/Assets/Scripts/CharacterName.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class CharacterName : NetworkBehaviour
 {
+    [SyncVar(hook = nameof(OnPlayerNameChanged))]
     private string playerName = "";
 
     [SerializeField] private TMP_Text playerNameText = default;
 
+    private void OnPlayerNameChanged(string oldName, string newName)
+    {
+        playerNameText.text = newName;
+    }
 
+    [Server]
     public void SetPlayerName(string newName)
     {
         playerName = newName;
+    }
+
+    public override void OnStartClient()
+    {
         playerNameText.text = playerName;
     }
 }
